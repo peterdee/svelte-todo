c02d9ebb41b18c31ac837aab8cb0e4e3c3f8106a
peterorbb <orbitalist@gmail.com>
2020-01-06 23:28:49 +0300

    Add footer, add Snyk badge

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Footer from './components/Footer.svelte';
 	import TodoIndex from './views/Todo/Index.svelte';
 
 	export let existingTodos = [];
@@ -7,13 +8,14 @@
 <main>
 	<TodoIndex todos="{existingTodos}" />
 </main>
+<Footer />
 
 <style>
 	main {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
-		min-height: 100vh;
+		min-height: calc(100vh - 30px);
 		text-align: center;
 		max-width: 440px;
 		margin: 0 auto;
---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<div class="footer">
+  With <span
+    class="highlight"
+  >❤</span> by <a
+    href="https://github.com/peterdee"
+  >Peter Dyumin</a>
+</div>
+
+<style>
+  .footer {
+    font-size: 12px;
+    height: 30px;
+    text-align: center;
+  }
+  .highlight {
+    color: #ff3e00;
+  }
+</style>
