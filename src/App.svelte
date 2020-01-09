54e5e1a9d4bb38546d7bc61ca97949a1f3a145f9
Peter <p.dyumin@dunice.net>
2020-01-09 13:36:31 +0300

    Formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,21 @@
 <script>
-	import Footer from './components/Footer.svelte';
-	import TodoIndex from './TodoApp/Index.svelte';
+  import Footer from './components/Footer.svelte';
+  import TodoIndex from './TodoApp/Index.svelte';
 </script>
 
 <main>
-	<TodoIndex />
+  <TodoIndex />
 </main>
 <Footer />
 
 <style>
-	main {
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-start;
-		min-height: calc(100vh - 30px);
-		text-align: center;
-		max-width: 440px;
-		margin: 0 auto;
-	}
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    min-height: calc(100vh - 30px);
+    text-align: center;
+    max-width: 440px;
+    margin: 0 auto;
+  }
 </style>
